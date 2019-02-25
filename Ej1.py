7d6920d82fc8a474d773f5665db826ac2fdaf9f9
Mauro870 <47693975+Mauro870@users.noreply.github.com>
2019-02-24 20:45:59 -0500

    Create Ej1.py

---
+++ Ej1.py
@@ -0,0 +1,13 @@
+def invertir_lista(unsort):
+    mayusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    string=list(unsort)
+    string_inv=list(unsort)
+    for i in range(len(string)-1,-1,-1):
+        if string[i] in mayusculas:
+            string[i]=string[i].lower()
+        else:
+            string[i]=string[i].upper()
+        string_inv[(len(string)-1)-i]=string[i]
+    return string_inv
+unsort = input('Introduce una cadena de texto: ')
+invertir_lista(unsort)
